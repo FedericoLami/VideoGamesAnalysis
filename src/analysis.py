a336e92e121c96467863facae1d10f6e1e4548c2
Federico <federicolami@hotmail.com>
2026-05-06 16:03:11 -0300

    agrego analisis de estructura del mercado

--- src/analysis.py
+++ src/analysis.py
@@ -4,6 +4,7 @@ from pathlib import Path
 def retornarPath():
     BASE_DIR = Path(__file__).resolve().parent.parent
     path = BASE_DIR / "data" / "vgsales_processed.csv"
+    return path
 
 df = pd.read_csv(retornarPath())
 
@@ -33,7 +34,7 @@ def return_Top_Platforms(col_name,region):
 
 #Bloque 3: Publishers en profundidad
 
-def return_Top_Publishers_Genre():
+def return_Top_Publishers():
     df_top_Publishers = df.groupby("Publishers")["Genre"].sum().sort_values(ascending = False).head(3)
     return df_top_Publishers
 
@@ -45,3 +46,49 @@ def return_Top_Publishers_Genre():
     return df_result[df_result>1]
 
 
+#Bloque 4: Estructura del mercado
+
+def return_Market_Distribution():
+    total_ventas = df["Global_Sales"].sum()
+    total_juegos = len(df)
+    df_sorted = df.sort_values("Global_Sales", ascending = False).reset_index(drop = True)
+    df_sorted["Ventas_Acumuladas"] = df_sorted["Global_Sales"].cumsum()
+    df_sorted["P_Ventas_Acum"] = df_sorted["Ventas_Acumuladas"] / total_ventas * 100
+    df_sorted["P_juegos_Acum"] = (df_sorted.index + 1) / total_juegos * 100
+    tramos = [1,5,10,25,50]
+    resultados = []
+
+    for pct in tramos:
+        n_juegos = int(total_juegos * pct / 100)
+        ventas_tramo = df_sorted.head(n_juegos)["Global_Sales"].sum()
+        resultados.append({
+            "top_%": pct,
+            "n_juegos": n_juegos,
+            "ventas_M": round(ventas_tramo, 2),
+            "% del total": round(ventas_tramo / total_ventas * 100, 1)
+        })
+
+        return pd.DataFrame(resultados)
+    
+def return_concentracion_top10():
+    df_sorted = df.sort_values("Global_Sales", ascending=False)
+    top10 = df_sorted.head(10)
+    resto = df_sorted.iloc[10:]
+
+    total = df["Global_Sales"].sum()
+
+    resultado = pd.DataFrame([
+        {
+            "grupo": "Top 10 juegos",
+            "n_juegos": 10,
+            "ventas_M": round(top10["Global_Sales"].sum(), 2),
+            "% del total": round(top10["Global_Sales"].sum() / total * 100, 1)
+        },
+        {
+            "grupo": "Resto",
+            "n_juegos": len(resto),
+            "ventas_M": round(resto["Global_Sales"].sum(), 2),
+            "% del total": round(resto["Global_Sales"].sum() / total * 100, 1)
+        }
+    ])
+    return resultado

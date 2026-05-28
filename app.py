410d77c0f60dccc51cb62ca0e97ded12d44da576
Federico <federicolami@hotmail.com>
2026-05-28 18:33:23 -0300

    feat: agrego tabla top 10 concentracion al dashboard

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
-from src.analysis import return_Top10_global_Sales
+from src.analysis import return_Top10_global_Sales, return_concentracion_top10
 
 
 st.title("Analisis de videojuegos")
-
 st.dataframe(return_Top10_global_Sales())
+
+st.subheader("Concentracion del top 10 de juegos")
+st.dataframe(return_concentracion_top10())

8f3861989dca4abe373d8c9c727d7fdd6e82e22c
Federico <federicolami@hotmail.com>
2026-05-28 18:36:27 -0300

    feat: agrego tabla de distribucion de mercado al dashboard

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from src.analysis import return_Top10_global_Sales, return_concentracion_top10
+from src.analysis import return_Top10_global_Sales, return_concentracion_top10, return_Market_Distribution
 
 
 st.title("Analisis de videojuegos")
@@ -7,3 +7,6 @@ st.dataframe(return_Top10_global_Sales())
 
 st.subheader("Concentracion del top 10 de juegos")
 st.dataframe(return_concentracion_top10())
+
+st.subheader("Distribucion de mercado")
+st.dataframe(return_Market_Distribution())

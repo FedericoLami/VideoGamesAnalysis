73484782ad745898a69a271def2094455a058da1
Federico <federicolami@hotmail.com>
2026-05-30 12:49:14 -0300

    feat: se amplia analisis por regiones

--- app.py
+++ app.py
@@ -13,29 +13,56 @@ st.subheader("Distribucion de mercado")
 st.dataframe(return_Market_Distribution())
 
 
-st.title("Analisis mercado global")
+st.header("Analisis mercado global")
 
-st.title("Mejor plataforma")
+st.subheader("Mejor plataforma")
 st.dataframe(return_Top_Bloq1("Platform"))
-st.title("Mejor publisher")
+st.subheader("Mejor publisher")
 st.dataframe(return_Top_Bloq1("Publisher"))
-st.title("Mejor genero")
+st.subheader("Mejor genero")
 st.dataframe(return_Top_Bloq1("Genre"))
 
 
 st.title("Analisis por region")
 
-st.title("Region NA")
-st.dataframe(return_Top_Bloq2())
-st.title("Region EU")
-st.dataframe(return_Top_Bloq2())
-st.title("Region JP")
-st.dataframe(return_Top_Bloq2())
-st.title("Otras regiones")
+st.header("Region NA")
+
+st.subheader("Top plataformas en NA")
+st.dataframe(return_Top_Bloq2("Platform","NA_sales"))
+st.subheader("Top publishers en NA")
+st.dataframe(return_Top_Bloq2("Publishers","NA_sales"))
+st.subheader("Top generos en NA")
+st.dataframe(return_Top_Bloq2("Genre","Na_sales"))
 
 
-st.title("Analisis de publishers en profundidad")
-st.title("Top publishers")
+st.header("Region EU")
+st.subheader("Top plataformas en NA")
+st.dataframe(return_Top_Bloq2("Platform","EU_sales"))
+st.subheader("Top publishers en NA")
+st.dataframe(return_Top_Bloq2("Publishers","EU_sales"))
+st.subheader("Top generos en NA")
+st.dataframe(return_Top_Bloq2("Genre","EU_sales"))
+
+st.header("Region JP")
+st.subheader("Top plataformas en JP")
+st.dataframe(return_Top_Bloq2("Platform","JP_sales"))
+st.subheader("Top publishers en JP")
+st.dataframe(return_Top_Bloq2("Publishers","JP_sales"))
+st.subheader("Top generos en JP")
+st.dataframe(return_Top_Bloq2("Genre","JP_sales"))
+
+
+st.header("Otras regiones")
+st.subheader("Top plataformas en otras regiones")
+st.dataframe(return_Top_Bloq2("Platform","Other_sales"))
+st.subheader("Top publishers en JP")
+st.dataframe(return_Top_Bloq2("Publishers","Other_sales"))
+st.subheader("Top generos en JP")
+st.dataframe(return_Top_Bloq2("Genre","Other_sales"))
+
+
+st.header("Analisis de publishers en profundidad")
+st.subheader("Top publishers")
 st.dataframe(return_Top_Publishers())
-st.title("Top publishers por genero")
+st.subheader("Top publishers por genero")
 st.dataframe(return_Top_Publishers_Genre())
